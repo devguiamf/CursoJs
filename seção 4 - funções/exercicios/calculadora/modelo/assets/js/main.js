e2ab65d369ff4b7f41c795f75480fcd0f7ad7f4e
Guilherme de Almeida Morais Ferreira <88168623+GuiAlmeidaMF@users.noreply.github.com>
2022-07-14 08:10:51 -0300

    14/07/22

---
+++ seção 4 - funções/exercicios/calculadora/modelo/assets/js/main.js
@@ -0,0 +1,60 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),       
+        
+        inicia(){
+            this.botoes()            
+        },
+
+        realizaConta(){
+            let conta = this.display.value
+            
+            try {
+                conta = eval(conta)
+
+                if(!conta){
+                    alert('conta invalida')
+                    return
+                }
+
+                this.display.value = conta
+            } catch (e) {
+                alert('conta invalida')
+                return
+            }
+        },
+
+        clearDisplay(){
+            this.display.value = ''
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(-1)
+        },
+        
+        botoes(){
+            document.addEventListener('click', function(e) {
+                const el = e.target
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText)
+                }
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay()
+                }
+                if (el.classList.contains('btn-dell')) {
+                    this.apagaUm()
+                }
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta()
+                }
+                
+            }.bind(this))
+        },
+        btnParaDisplay(valor){
+            this.display.value += valor
+        }
+    }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
--- seção 4 - funções/factory.js
+++ seção 4 - funções/factory.js
@@ -1,12 +1,19 @@
-function criaPessoa(nome, sobrenome) {
+function criaPessoa(nome, sobrenome, peso, altura) {
     return {
         nome: nome,
         sobrenome: sobrenome,
         fala: function(assunto) {// quando temos função dentro de um objeto chamamos de método
             return `${nome} esta falando do ${assunto}`
+        },
+        peso: peso,
+        altura: altura,
+        imc(){
+            const indice = this.peso / (this.altura ** 2)
+            return indice.toFixed(2)
         }
     };
 }
 
-const p1 = criaPessoa('Guilherme','Almeida')
+const p1 = criaPessoa('Guilherme','Almeida', 80, 1.74)
 console.log(p1.fala('Terraplanismo'));
+console.log(p1.imc());
